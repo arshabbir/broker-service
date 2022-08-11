bbcb869765380b206c34ec6bb5fbdd1f0858c06c
arshabbir <arshabbir@gmail.com>
2022-08-11 09:45:32 +0530

    added env variables

--- config/config.go
+++ config/config.go
@@ -1,11 +1,16 @@
 package config
 
+import (
+	"fmt"
+	"os"
+)
+
 type Config struct {
 	AppPort string `json:"appport"`
 }
 
 func (c *Config) LoadConfig() error {
 	// Implement viper load config
-	c.AppPort = ":8080"
+	c.AppPort = fmt.Sprintf(":%s", os.Getenv("APP_PORT"))
 	return nil
 }
